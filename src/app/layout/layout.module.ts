d4641eaaddab8b55a36592364239208ba594888a
juanan83 <juanangelpueyo@outlook.com>
2021-09-16 18:21:03 +0200

    Ejercicio con componentes cabecera, titulo y pie

---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CabeceraComponent } from './cabecera/cabecera.component';
+import { PieComponent } from './pie/pie.component';
+
+
+
+@NgModule({
+  declarations: [
+    CabeceraComponent,
+    PieComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class LayoutModule { }
