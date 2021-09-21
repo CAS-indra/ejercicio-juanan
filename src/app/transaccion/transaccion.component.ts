2caaf27cbf346edc5717e08ade1f72ed3beda9f7
juanan83 <juanangelpueyo@outlook.com>
2021-09-21 11:06:17 +0200

    Agregada la lógica de las transacciones.

--- src/app/transaccion/transaccion.component.ts
+++ src/app/transaccion/transaccion.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TransaccionesService } from '../data/transacciones.service';
 
 @Component({
   selector: 'app-transaccion',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./transaccion.component.scss']
 })
 export class TransaccionComponent implements OnInit {
+  public transaccionId = '';
+  public transaccion = {id: '', name: '', description: '', kind: '', amount: 0, date: '', projectId: '',  ownerId: ''};
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private service: TransaccionesService) { }
 
   ngOnInit(): void {
+    this.transaccionId = this.route.snapshot.params['id'];
+    const transacciones = this.service.getTransacciones();
+    this.transaccion = transacciones.find(p => p.id === this.transaccionId);
   }
 
 }
