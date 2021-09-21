34233014222a502f86934a0d37fb533942fd5f31
juanan83 <juanangelpueyo@outlook.com>
2021-09-21 11:04:52 +0200

    Modificación del path del módulo transaccion.

--- src/app/transaccion/transaccion-routing.module.ts
+++ src/app/transaccion/transaccion-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TransaccionComponent } from './transaccion.component';
 
-const routes: Routes = [{ path: '', component: TransaccionComponent }];
+const routes: Routes = [{ path: ':id', component: TransaccionComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
